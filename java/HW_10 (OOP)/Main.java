5dc176d38c78818d4ef72d8fe1e016d41e971ccf
dife <75038049+Difepay@users.noreply.github.com>
2022-03-25 14:21:24 +0300

    Add files via upload

---
+++ java/HW_10 (OOP)/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 6);
+
+        System.out.println("p1: " + p1.toString());
+    }
+}
