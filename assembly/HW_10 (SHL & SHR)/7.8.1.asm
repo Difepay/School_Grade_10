26df968f7bfc0b126c1930347648a85b8d69946b
dife <75038049+Difepay@users.noreply.github.com>
2022-03-31 12:53:56 +0300

    Update 7.8.1.asm

--- assembly/HW_10 (SHL & SHR)/7.8.1.asm
+++ assembly/HW_10 (SHL & SHR)/7.8.1.asm
@@ -12,27 +12,9 @@ start:
 	xor ax, ax
 	xor cl, cl
 	
-	; Task A
 	mov al, 3
 	mov cl, 2
 	shl al, cl
-	
-	; Task B
-	mov al, 120d
-	mov cl, 3
-	shr al, cl
-	
-	
-	; Task C
-	mov al, 10d
-	mov ah, 10d
-	
-	mov cl, 2
-	shl al, cl
-	mov cl, 4
-	shl ah, cl
-	add al, ah
-	xor ah, ah
 
 exit:
 	mov ax, 4c00h
