1cd616b07a9f1e74b701d8f71182b955524d6a75
dife <75038049+Difepay@users.noreply.github.com>
2022-03-31 12:49:06 +0300

    Create 7.4.1.asm

---
+++ assembly/HW_9 (DIV)/7.4.1.asm
@@ -0,0 +1,26 @@
+IDEAL
+MODEL small
+STACK 100h
+DATASEG
+	;-----VARS-----:
+	varl db 9
+	var2 db 4
+	result_quo db 0
+	result_rem db 0
+CODESEG
+start:
+	mov ax, @data
+	mov ds, ax
+   
+	;-----CODE-----:
+	xor ax, ax
+	mov al, [varl]
+	div [var2]
+	
+	mov [result_quo], al
+	mov [result_rem], ah
+	
+exit:
+	mov ax,4c00h
+	int 21h
+END start
